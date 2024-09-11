d5e798854119f54b0bf87cc2ca0015b5f9a91db3
Emin Novruz <emin.novruz@sinam.net>
2024-09-11 10:36:01 +0400

    New QrCode Service

---
+++ WebApi/Services/Abstract/IQrService.cs
@@ -0,0 +1,11 @@
+﻿using OtpNet;
+
+namespace WebApi.Services.Abstract;
+
+public interface IQrService
+{
+    string GenerateSecretKey();
+    string GenerateQrCodeUri(string email, string secretKey, string issuer = "YourAppName");
+    Task<string> GenerateQrCodeAsync(string email, string secretKey, string issuer = "YourAppName");
+    bool VerifyTotpCode(string secretKey, string userInputCode);
+}
---
+++ WebApi/Services/QrService.cs
@@ -0,0 +1,44 @@
+﻿using OtpNet;
+using QRCoder;
+using WebApi.Services.Abstract;
+
+namespace WebApi.Services;
+
+public class QrService : IQrService
+{
+    public string GenerateSecretKey()
+    {
+        var key = KeyGeneration.GenerateRandomKey(20);
+        return Base32Encoding.ToString(key);
+    }
+
+    public string GenerateQrCodeUri(string email, string secretKey, string issuer = "YourAppName")
+    {
+        return $"otpauth://totp/{issuer}:{email}?secret={secretKey}&issuer={issuer}&digits=6";
+    }
+
+    public async Task<string> GenerateQrCodeAsync(string email, string secretKey, string issuer = "YourAppName")
+    {
+        var uri = GenerateQrCodeUri(email, secretKey, issuer);
+
+        return await Task.Run(() =>
+        {
+            using (var qrGenerator = new QRCodeGenerator())
+            {
+                var qrCodeData = qrGenerator.CreateQrCode(uri, QRCodeGenerator.ECCLevel.Q);
+                using (var qrCode = new PngByteQRCode(qrCodeData))
+                {
+                    var qrCodeBytes = qrCode.GetGraphic(20);
+                    return Convert.ToBase64String(qrCodeBytes);
+                }
+            }
+        });
+    }
+
+    public bool VerifyTotpCode(string secretKey, string userInputCode)
+    {
+        var key = Base32Encoding.ToBytes(secretKey);
+        var totp = new Totp(key);
+        return totp.VerifyTotp(userInputCode, out long timeStepMatched, new VerificationWindow(2, 2));
+    }
+}
